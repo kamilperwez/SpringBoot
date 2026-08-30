97d64a2c6663ef8a8e529d770e1cab2ef5dbacd6
kamilperwez <104694084+kamilperwez@users.noreply.github.com>
2026-08-31 01:14:37 +0530

    crud

--- todo-manager/src/main/java/com/kamil/todo_manager/TodoManagerApplication.java
+++ todo-manager/src/main/java/com/kamil/todo_manager/TodoManagerApplication.java
@@ -1,13 +1,28 @@
 package com.kamil.todo_manager;
 
+import com.kamil.todo_manager.dao.TodoDao;
+import com.kamil.todo_manager.entity.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoManagerApplication {
+public class TodoManagerApplication implements CommandLineRunner {
+	@Autowired
+	private TodoDao todoDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoManagerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Todo todo=new Todo();
+		todo.setId(122);
+		todo.setTitle("Programmer");
+		todo.setContent("Learning SpringBoot");
+		todo.setStatus("In Progress");
+		todoDao.save(todo);
+	}
 }
--- todo-manager/src/main/java/com/kamil/todo_manager/dao/TodoDao.java
+++ todo-manager/src/main/java/com/kamil/todo_manager/dao/TodoDao.java
@@ -1,9 +1,36 @@
 package com.kamil.todo_manager.dao;
 
+import com.kamil.todo_manager.entity.Todo;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TodoDao {
     @Autowired
     private JdbcTemplate template;
+    Logger logger= LoggerFactory.getLogger(TodoDao.class);
+    public JdbcTemplate getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    public TodoDao(@Autowired JdbcTemplate template) {
+        this.template = template;
+        String createTable="create table if not exists todos (id int primary key, title varchar(30), content varchar(300), status varchar(15))";
+        template.update(createTable);
+        logger.info("Table created");
+    }
+    public void save(Todo todo){
+        String query="insert into todos values (?,?,?,?)";
+        int n= template.update(query,todo.getId(),todo.getTitle(),todo.getContent(),todo.getStatus());
+        logger.info("Row Inserted");
+    }
+
 }
